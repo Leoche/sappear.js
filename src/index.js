699e43cab30977b88b2377beb89355db18ae4a41
Léo DESIGAUX <leodesigaux@gmail.com>
2019-07-19 11:16:12 +0200

    Adds delay, easings, and duration parameters to SAP

--- src/index.js
+++ src/index.js
@@ -12,14 +12,58 @@ let SAppear = {
                 opacity:1,
                 translateY:0,
             }
+        },
+        'sap_scaleup':{
+            from:{
+                opacity:0,
+                scale:.5,
+            },
+            to:{
+                opacity:1,
+                scale:1,
+            }
+        },
+        'sap_slidedown':{
+            from:{
+                opacity:0,
+                translateY:-50,
+            },
+            to:{
+                opacity:1,
+                translateY:0,
+            }
+        },
+        'sap_slideright':{
+            from:{
+                opacity:0,
+                translateX:-50,
+            },
+            to:{
+                opacity:1,
+                translateX:0,
+            }
+        },
+        'sap_slideleft':{
+            from:{
+                opacity:0,
+                translateX:50,
+            },
+            to:{
+                opacity:1,
+                translateX:0,
+            }
         }
     },
+    easings: ['easeInQuad', 'easeOutQuad', 'easeInOutQuad','easeInCubic', 'easeOutCubic','easeInOutCubic','easeInQuart', 'easeOutQuart','easeInOutQuart','easeInQuint', 'easeOutQuint','easeInOutQuint','easeInSine', 'easeOutSine', 'easeInOutSine','easeInExpo', 'easeOutExpo', 'easeInOutExpo','easeInCirc', 'easeOutCirc', 'easeInOutCirc','easeInBack', 'easeOutBack', 'easeInOutBack'],
     init(obj){
         if(!this.selectors) return;
         this.selectors.forEach(elem => {
             let newElem = {
                 elem:elem,
                 anim:this.animations.sap_slideup,
+                delay:0,
+                duration: 600,
+                easing:"easeOutBack",
                 appeared:false
             }
             for (var key in this.animations) {
@@ -27,6 +71,25 @@ let SAppear = {
                     newElem.anim = this.animations[key]
                 }
             }
+
+            elem.classList.forEach(className => {
+                let regex = /sap_duration_(\d+)/g;
+                let match = regex.exec(className);
+                if (match !== null && match[1]) {
+                    newElem.duration = parseInt(match[1]) * 100;
+                }
+                regex = /sap_delay_(\d+)/g;
+                match = regex.exec(className);
+                if (match !== null && match[1]) {
+                    newElem.delay = parseInt(match[1]) * 100;
+                }
+                regex = /sap_easing_([A-z]+)/g;
+                match = regex.exec(className);
+                if (match !== null && match[1]) {
+                    newElem.easing = match[1];
+                }
+            })
+            
             let animeInit = {
                 targets: newElem.elem,
                 duration:0,
@@ -41,8 +104,11 @@ let SAppear = {
                 if(elem.intersectionRatio > 0) {
                     let animeInit = {
                         targets: newElem.elem,
-                        duration:2000,
+                        easing: newElem.easing,
+                        delay: newElem.delay,
+                        duration:newElem.duration,
                     }
+                    console.log(animeInit);
                     for (var key in  newElem.anim.to){
                         animeInit[key] = newElem.anim.to[key];
                     }
@@ -50,7 +116,7 @@ let SAppear = {
                     observer.unobserve(newElem.elem)
                 }
               });
-            }, { rootMargin: '0px 0px', threshold: 0.00 });
+            }, { rootMargin: '-100px -100px', threshold: 0.50 });
 
             observer.observe(newElem.elem);
         })
